3de46ad2a01221ef64a1a77654b164671ed23ea6
LucasKaiquee <lucasKaique743@gmail.com>
2023-04-20 15:08:48 -0300

    Correção

--- exercicio-repeticao-while/exe4.py
+++ exercicio-repeticao-while/exe4.py
@@ -1,17 +1,6 @@
 matricula = int(input("Digite a matricula do aluno"))
-nome = input("Digite o nome do aluno:")
-nota1 = float(input("Digite a primeira nota do aluno"))
-nota2 = float(input("Digite a nota do aluno"))
-
-media = (nota1 + nota2) /2
-
-if media >= 7:
-    print(f"O aluno: {nome}, matricula: {matricula}, está aprovado com média {media}")
-else:
-    print(f"O aluno: {nome}, matricula: {matricula}, está reprovado com média {media}")
 
 while matricula != 0:
-    matricula = int(input("Digite a matricula do aluno"))
     nome = input("Digite o nome do aluno:")
     nota1 = float(input("Digite a primeira nota do aluno"))
     nota2 = float(input("Digite a nota do aluno"))
@@ -22,3 +11,4 @@ while matricula != 0:
         print(f"O aluno: {nome}, matricula: {matricula}, está aprovado com média {media}")
     else:
         print(f"O aluno: {nome}, matricula: {matricula}, está reprovado com média {media}")
+    matricula = int(input("Digite a matricula do aluno"))
