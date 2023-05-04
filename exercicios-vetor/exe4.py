052526d8131832893780799f2fa2205ab3185bf3
Lucas Kaique Valerio Soares <20231370019@ad.ifpb.edu.br>
2023-05-04 14:28:01 -0300

    vetor exe4 mudando valor

--- exercicios-vetor/exe4.py
+++ exercicios-vetor/exe4.py
@@ -1,12 +1,12 @@
-v = [None]*5
+v = [None]*10
 
-for i in range(5):
+for i in range(10):
     v[i] = int(input("Digite o valor"))
 
-for j in range(5):
+for j in range(10):
     if j%2 == 0:
         print(f"Pares: o valor está no indice {j} : {v[j]}")
  
-for k in range(5):
+for k in range(10):
     if k%2 != 0:
         print(f"Impares: o valor está no indice {k} : {v[k]}")
